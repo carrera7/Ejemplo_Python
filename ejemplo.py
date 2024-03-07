bc488532244016623d2fa4add3ba1dbb3eebba59
carrera7 <josuecarrera788@gmail.com>
2024-03-07 00:46:47 -0300

    agrege funcion

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,11 @@
-print("Hola mundo")
+def pregunta(respuesta):
+    if respuesta.lower() == "bien":
+        print("Me alegro que estes bien, Bendiciones")
+    else:
+        print("El dia todoavia no acaba, puede mejorar")    
+
+
+persona = "Josue"
+print("Hola" , persona , "como estas")
+respuesta = input("Ingrese el estado de animo \n")
+pregunta(respuesta)
